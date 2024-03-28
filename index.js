ee0c8b2a1d760ebedcbd960505909cfef60ec232
jkugsiya <jkugsiya11@gmail.com>
2024-03-28 16:36:27 +0530

    date logs

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ app.use(express.json());
 
 app.post("/timezone", (req, res) => {
   const date = new Date(req.body.date);
-  const serverDate = new Date();
   date.setHours(23, 59, 59, 99);
+  const serverDate = new Date();
+  console.log({
+    dateOffset: date.getTimezoneOffset(),
+    serverDateOffset: serverDate.getTimezoneOffset(),
+  });
+  if (serverDate.getTimezoneOffset() !== date.getTimezoneOffset()) {
+    date.setTime(date.getTime() + date.getTimezoneOffset() * 60 * 1000);
+  }
+
   res.send(
     JSON.stringify({
       date,
